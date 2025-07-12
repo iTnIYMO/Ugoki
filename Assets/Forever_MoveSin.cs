cfba733af2d7e899bd5bd23141242e1d354da248
iTnIYMO <minamiosawap@gmail.com>
2025-07-12 19:37:44 +0900

    Fix : Debug.Log() TEST
    Debug.Log() が表示されなかった

--- Assets/Forever_MoveSin.cs
+++ Assets/Forever_MoveSin.cs
@@ -8,7 +8,7 @@ public class Forever_MoveSin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(time);
     }
 
     // Update is called once per frame
